01759fc16224494f366b80cb34ad437919b0d5cf
Akul Singhal <singhal.akul9@gmail.com>
2020-04-08 15:00:51 +0530

    add option to select default language

--- companionHandler.js
+++ companionHandler.js
@@ -1,6 +1,7 @@
 const vscode = require("vscode");
 const parseTestCasesFile = require("./parseTestCasesFile");
 const createTestacesFile = require("./createTestcasesFile");
+const preferences = require("./preferencesHelper"); 
 const locationHelper = require("./locationHelper");
 const path = require("path");
 const fs = require("fs");
@@ -22,9 +23,24 @@ function handleCompanion(problem) {
         const dir = vscode.workspace.workspaceFolders[0].uri.fsPath;
         const languageChoices = Object.keys(config.extensions);
 
-        vscode.window.showQuickPick(languageChoices, {
-            placeHolder: "Select the language"
-        })
+        const defaultLanguage = preferences().get("defaultLanguage");
+        let selectLanguage;
+        if(defaultLanguage != "") {
+            selectLanguage = new Promise( (resolve) => {
+                resolve(defaultLanguage);
+            });
+        }
+        else {
+            selectLanguage = new Promise( (resolve) => {
+                vscode.window.showQuickPick(languageChoices, {
+                    placeHolder: "Select the language"
+                })
+                .then(async language => {
+                    resolve(language);
+                })
+            });
+        }
+        selectLanguage
         .then(async language => {
             const ext = config.extensions[language];
             if (!ext)
