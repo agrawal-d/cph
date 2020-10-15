ff63f18997b10140663c21c1a44d27ec5f48529a
arnav-ag <arnav.ctf@gmail.com>
2020-10-15 19:24:16 +0800

    Fix resolve conflicts

---
+++ src/compiler.ts
@@ -0,0 +1,127 @@
+import { getLanguage, ocHide, ocShow, ocWrite } from '../utils';
+import { Language } from '../types';
+import { spawn } from 'child_process';
+import path from 'path';
+import { getSaveLocationPref } from '../preferences';
+import { extensionToWebWiewMessage } from '../webview';
+import * as vscode from 'vscode';
+
+/**
+ *  Get the location to save the generated binary in. If save location is
+ *  available in preferences, returns that, otherwise returns the director of
+ *  active file.
+ *
+ *  If it is a interpteted language, simply returns the path to the source code.
+ *
+ *  @param srcPath location of the source code
+ */
+export const getBinSaveLocation = (srcPath: string): string => {
+    const language = getLanguage(srcPath);
+    if (language.skipCompile) {
+        return srcPath;
+    }
+    const savePreference = getSaveLocationPref();
+    const srcFileName = path.basename(srcPath);
+    const binFileName = `${srcFileName}.bin`;
+    if (savePreference && savePreference !== '') {
+        return path.join(savePreference, binFileName);
+    }
+    return `${srcPath}.bin`;
+};
+
+/**
+ * Get the complete lsit of required arguments to be passed to the compiler.
+ * Loads additional args from preferences if available.
+ *
+ * @param language The Language object for the source code
+ * @param srcPath location of the source code
+ */
+const getFlags = (language: Language, srcPath: string): string[] => {
+    // The language.args are fetched from user saved preferences, if any.
+    let args = language.args;
+    if (args[0] === '') args = [];
+
+    switch (language.name) {
+        case 'cpp': {
+            return [
+                srcPath,
+                '-o',
+                getBinSaveLocation(srcPath),
+                ...args,
+                '-D',
+                'ONLINE_JUDGE',
+                '-D',
+                'CPH',
+            ];
+        }
+        case 'c': {
+            {
+                return [srcPath, '-o', getBinSaveLocation(srcPath), ...args];
+            }
+        }
+        case 'rust': {
+            return [srcPath, '-o', getBinSaveLocation(srcPath), ...args];
+        }
+        default: {
+            return [];
+        }
+    }
+};
+
+/**
+ * Compile a source file, storing the output binary in a location based on user
+ * preferences. If `skipCompile` is true for a language, skips the compilation
+ * and resolves true. If there is no preference, stores in the current
+ * directory. Resolves true if it succeeds, false otherwise.
+ *
+ * @param srcPath location of the source code
+ */
+export const compileFile = (srcPath: string): Promise<boolean> => {
+    console.log('Compilation Started');
+    ocHide();
+    const language: Language = getLanguage(srcPath);
+    if (language.skipCompile) {
+        return Promise.resolve(true);
+    }
+    extensionToWebWiewMessage({
+        command: 'compiling-start',
+    });
+    const flags: string[] = getFlags(language, srcPath);
+    console.log('Compiling with flags', flags);
+    const result = new Promise<boolean>((resolve) => {
+        let compiler;
+        try {
+            compiler = spawn(language.compiler, flags);
+        } catch (err) {
+            vscode.window.showErrorMessage(
+                `Could not launch the compiler ${language.compiler}. Is it installed?`,
+            );
+            throw err;
+        }
+        let error = '';
+
+        compiler.stderr.on('data', (data) => {
+            error += data;
+        });
+
+        compiler.on('exit', (exitcode) => {
+            if (exitcode === 1 || error !== '') {
+                ocWrite('Errors while compiling:\n' + error);
+                ocShow();
+                console.error('Compilation failed');
+                resolve(false);
+                extensionToWebWiewMessage({
+                    command: 'compiling-stop',
+                });
+                return;
+            }
+            console.log('Compilation passed');
+            extensionToWebWiewMessage({
+                command: 'compiling-stop',
+            });
+            resolve(true);
+            return;
+        });
+    });
+    return result;
+};
---
+++ src/executions.ts
@@ -0,0 +1,129 @@
+import { Language, Run } from '../types';
+import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
+import { platform } from 'os';
+import config from '../config';
+import { getTimeOutPref } from '../preferences';
+import * as vscode from 'vscode';
+
+const runningBinaries: ChildProcessWithoutNullStreams[] = [];
+
+/**
+ * Run a single testcase, and return the raw results, without judging.
+ *
+ * @param binPath path to the executable binary
+ * @param input string to be piped into the stdin of the spawned process
+ */
+export const runTestCase = (
+    language: Language,
+    binPath: string,
+    input: string,
+): Promise<Run> => {
+    console.log('Running testcase', language, binPath, input);
+    const result: Run = {
+        stdout: '',
+        stderr: '',
+        code: null,
+        signal: null,
+        time: 0,
+        timeOut: false,
+    };
+    const spawnOpts = {
+        timeout: config.timeout,
+        env: { ONLINE_JUDGE: 'true', DEBUG: 'true', CPH: 'true' },
+    };
+    let process: ChildProcessWithoutNullStreams;
+
+    const killer = setTimeout(() => {
+        result.timeOut = true;
+        process.kill();
+    }, getTimeOutPref());
+
+    // Start the binary or the interpreter.
+    switch (language.name) {
+        case 'python': {
+            process = spawn(
+                language.compiler, // 'python3' or 'python' TBD
+                [binPath, ...language.args],
+                spawnOpts,
+            );
+            break;
+        }
+        default: {
+            process = spawn(binPath, spawnOpts);
+        }
+    }
+
+    process.on('error', (err) => {
+        console.error(err);
+        vscode.window.showErrorMessage(
+            `Could not launch testcase process. Is '${language.compiler}' in your PATH?`,
+        );
+    });
+
+    const begin = Date.now();
+    const ret: Promise<Run> = new Promise((resolve) => {
+        runningBinaries.push(process);
+        process.on('exit', (code, signal) => {
+            const end = Date.now();
+            clearTimeout(killer);
+            result.code = code;
+            result.signal = signal;
+            result.time = end - begin;
+            runningBinaries.pop();
+            console.log('Run Result:', result);
+            resolve(result);
+        });
+
+        process.stdout.on('data', (data) => {
+            result.stdout += data;
+        });
+        process.stderr.on('data', (data) => (result.stderr += data));
+
+        console.log('Wrote to STDIN');
+        try {
+            process.stdin.write(input);
+        } catch (err) {
+            console.error('WRITEERROR', err);
+        }
+
+        process.stdin.end();
+        process.on('error', (err) => {
+            const end = Date.now();
+            clearTimeout(killer);
+            result.code = 1;
+            result.signal = err.name;
+            result.time = end - begin;
+            runningBinaries.pop();
+            console.log('Run Error Result:', result);
+            resolve(result);
+        });
+    });
+
+    return ret;
+};
+
+/** Remove the generated binary from the file system, if present */
+export const deleteBinary = (language: Language, binPath: string) => {
+    if (language.skipCompile) {
+        console.log(
+            "Skipping deletion of binary as it's not a compiled language.",
+        );
+        return;
+    }
+    console.log('Deleting binary', binPath);
+    try {
+        if (platform() == 'linux') {
+            spawn('rm', [binPath]);
+        } else {
+            spawn('del', [binPath]);
+        }
+    } catch (err) {
+        console.error('Error while deleting binary', err);
+    }
+};
+
+/** Kill all running binaries. Usually, only one should be running at a time. */
+export const killRunning = () => {
+    console.log('Killling binaries');
+    runningBinaries.forEach((process) => process.kill());
+};
