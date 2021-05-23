71243ca756973ee20fee1d29561de9fc5bfe18a1
Aryan Agarwal <55305876+aryan57@users.noreply.github.com>
2021-05-23 15:21:44 +0530

    add parsed time

--- src/companion.ts
+++ src/companion.ts
@@ -199,7 +199,26 @@ const handleNewProblem = async (problem: Problem) => {
         id: randomId(),
     }));
     if (!existsSync(srcPath)) {
-        writeFileSync(srcPath, '');
+
+        let date_ob = new Date();
+
+        // current date
+        // adjust 0 before single digit date
+        let date = ("0" + date_ob.getDate()).slice(-2);
+
+        // current month
+        let month_name = date_ob.toLocaleString('default', { month: 'long' });
+
+        // current year
+        let year = date_ob.getFullYear();
+
+        let today_string = date + "-" + month_name + "-" + year + " " + date_ob.toLocaleTimeString('en-US', { hour12: false });
+
+        let str='';
+        str+='\n';
+        str+='//\tparsed : '+today_string+' IST';
+
+        writeFileSync(srcPath, str);
     }
     saveProblem(srcPath, problem);
     const doc = await vscode.workspace.openTextDocument(srcPath);
