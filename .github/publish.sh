5617d877b491d937447a39b314d2d7f06f6699f2
Divyanshu Agrawal <agrawal-d@outlook.com>
2024-06-04 22:02:24 +0530

    Publish workflow.

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 # Publish a new version of the extension to the marketplace
 
+# IF no pat exit
+if [ -z "$VSCE_PAT" ]; then
+  echo "VSCE_PAT is not set. Exiting."
+  exit 1
+fi
+
+# Print first 3 and last 3 chars of PAT
+echo "VSCE_PAT: $(echo $VSCE_PAT | cut -c1-3)***$(echo $VSCE_PAT | rev | cut -c1-3 | rev)"
+
 set -e
 set -x
 
-vsce login DivyanshuAgrawal <<< "$VSCE_PAT"
-
 year=$(date +%Y)
-month=$(date +%m)
+month=$(date +%-m)
 epoch=$(date +%s)
 version="$year.$month.$epoch"
 
