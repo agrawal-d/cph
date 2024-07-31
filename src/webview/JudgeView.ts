63977514a5cc021f181cb86a1a482f6bccb8f904
Divyanshu Agrawal <agrawal-d@outlook.com>
2024-07-31 18:17:44 +0530

    Fix problem on Linux.

--- src/webview/JudgeView.ts
+++ src/webview/JudgeView.ts
@@ -208,6 +208,10 @@ class JudgeViewProvider implements vscode.WebviewViewProvider {
             ),
         );
 
+        const remoteMessage = globalThis.remoteMessage
+            ? globalThis.remoteMessage.trim()
+            : ' ';
+
         const html = `
             <!DOCTYPE html lang="EN">
             <html>
@@ -228,11 +232,7 @@ class JudgeViewProvider implements vscode.WebviewViewProvider {
                         // Since the react script takes time to load, the problem is sent to the webview before it has even loaded.
                         // So, for the initial request, ask for it again.
                         window.vscodeApi = acquireVsCodeApi();
-                        window.remoteMessage = '${
-                            globalThis.remoteMessage
-                                ? globalThis.remoteMessage
-                                : ' '
-                        }';
+                        window.remoteMessage = '${remoteMessage}';
                         document.addEventListener(
                             'DOMContentLoaded',
                             (event) => {
