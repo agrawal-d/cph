15f9641d348c01c30285466f6ae1a3948e4c9854
AksLolCoding <95454315+AksLolCoding@users.noreply.github.com>
2025-01-19 12:27:27 -0500

    fix eslint 4

--- src/webview/frontend/App.tsx
+++ src/webview/frontend/App.tsx
@@ -561,7 +561,8 @@ function Judge(props: {
                 {window.showLiveUserCount && (
                     <>
                         <h3>Live user count</h3>
-                        {liveUserCount} {liveUserCount === 1 ? 'user' : 'users'} online.
+                        {liveUserCount} {liveUserCount === 1 ? 'user' : 'users'}{' '}
+                        online.
                         <hr />
                     </>
                 )}
@@ -654,7 +655,8 @@ function Judge(props: {
                 {window.showLiveUserCount && liveUserCount > 0 && (
                     <div className="liveUserCount">
                         <i className="codicon codicon-circle-filled color-green"></i>{' '}
-                        {liveUserCount} {liveUserCount === 1 ? 'user' : 'users'}{' '} online.
+                        {liveUserCount} {liveUserCount === 1 ? 'user' : 'users'}{' '}
+                        online.
                     </div>
                 )}
             </div>
